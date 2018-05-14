9a74fac9395773e7cd9ec847217f2a457f09a72b
Olgerd Ševiakov <olgerd@zenitech.co.uk>
2018-05-14 15:17:04 +0300

    OK, test logic is clear now

--- src/test/java/org/cynic/megaapplication/SampleControllerTest.java
+++ src/test/java/org/cynic/megaapplication/SampleControllerTest.java
@@ -1,12 +1,39 @@
 package org.cynic.megaapplication;
 
+import org.cynic.megaapplication.controller.SampleController;
+import org.cynic.megaapplication.service.SampleService;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class SampleControllerTest {
+    @InjectMocks
+    private SampleController sampleController;
+    @Mock
+    private SampleService sampleService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
-    public void randomizerTest() {
+    public void shouldEcho() {
+        int result =  sampleController.echo("other");
+        assertEquals(6, result);
+    }
 
+    @Test
+    public void shouldEchoUsingService() {
+        Mockito.when(sampleService.process(Mockito.eq("banana"))).thenReturn("ResultIs9");
+        int result = sampleController.echo("banana");
+        assertEquals(9, result);
+        Mockito.verify(sampleService).process(Mockito.eq("banana"));
     }
 }
